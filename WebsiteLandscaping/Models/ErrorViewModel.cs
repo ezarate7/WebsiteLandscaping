7de829a9c6111c31df013f825f6f60dc4970ac96
ezarate7 <ezarate7@outlook.com>
2022-10-06 19:09:43 -0600

    Add project files.

---
+++ WebsiteLandscaping/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteLandscaping.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
